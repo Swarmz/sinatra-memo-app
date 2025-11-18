483d081546c0488663ed9687c8bc52d06ea4851d
Swarmz <gavinsutton6511@gmail.com>
2025-11-18 18:30:54 +0900

    Sinatraメモアプリの初期版を追加

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# sinatra-memo-app
+## メモアプリ
+
 Sinatraで作成したシンプルなメモ管理アプリ
+
+## 必要環境
+
+* Ruby（使用しているバージョン例：3.3.10）
+* Bundler
+
+## アプリの起動手順
+
+1. リポジトリをクローン
+
+    ```
+    git clone https://github.com/Swarmz/sinatra-memo-app.git
+    cd sinatra-memo-app
+    ```
+
+2. 依存関係をインストール
+
+    ```
+    bundle install
+    ```
+
+3. アプリを起動
+
+    ```
+    ruby memo_app.rb
+    ```
+
+    または、`rerun` を使用する場合：
+
+    ```
+    bundle exec rerun memo_app.rb
+    ```
+
+4. ブラウザからアクセス
+
+    ```
+    http://localhost:4567
+    ```
---
+++ memo_app.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'json'
+require 'securerandom'
+require 'rack/protection'
+
+enable :method_override
+use Rack::Protection
+
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
+
+def store_memo(filename, title, body)
+  memos = JSON.parse(File.read(filename))
+  memos << { 'id' => SecureRandom.uuid, 'title' => title, 'body' => body }
+  File.write(filename, JSON.pretty_generate(memos))
+  memos.last
+end
+
+def read_memo_list
+  File.open('memo_list.json', 'a') { |file| file.puts '[]' } unless File.exist?('memo_list.json')
+  JSON.parse(File.read('memo_list.json'))
+end
+
+def delete_memo(filename, id)
+  memos = JSON.parse(File.read(filename))
+  memos.reject! { |memo| memo['id'] == id }
+  File.write(filename, JSON.pretty_generate(memos))
+end
+
+before do
+  @memos = read_memo_list
+end
+
+before '/memos/:id*' do
+  # '/memos/new' の場合はフィルターをスキップし、それ以外はメモを探して見つからなければ 404 を返す
+  pass if params[:id] == 'new'
+  @memo = @memos.find { |memo| memo['id'] == params[:id] }
+  halt 404, erb(:not_found) unless @memo
+end
+
+get '/' do
+  redirect '/memos'
+end
+
+get '/memos' do
+  erb :index
+end
+
+get '/memos/new' do
+  erb :new
+end
+
+post '/memos' do
+  memo = store_memo('memo_list.json', params['title'], params['body'])
+  redirect "/memos/#{memo['id']}"
+end
+
+get '/memos/:id' do
+  erb :show
+end
+
+get '/memos/:id/edit' do
+  erb :edit
+end
+
+patch '/memos/:id' do
+  @memo['title'] = params['title']
+  @memo['body']  = params['body']
+  File.write('memo_list.json', JSON.pretty_generate(@memos))
+  redirect "/memos/#{@memo['id']}"
+end
+
+delete '/memos/:id' do
+  delete_memo('memo_list.json', @memo['id'])
+  redirect '/'
+end
+
+not_found do
+  erb :not_found
+end
