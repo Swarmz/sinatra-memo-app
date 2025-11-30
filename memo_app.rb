290510bb3d733de92ad472d3c4db37b8f773d23a
Swarmz <gavinsutton6511@gmail.com>
2025-11-30 17:49:51 +0900

    メモデータのハッシュキーを文字列からシンボルに統一

--- memo_app.rb
+++ memo_app.rb
@@ -14,20 +14,20 @@ helpers do
 end
 
 def store_memo(filename, title, body)
-  memos = JSON.parse(File.read(filename))
-  memos << { 'id' => SecureRandom.uuid, 'title' => title, 'body' => body }
+  memos = JSON.parse(File.read(filename), symbolize_names: true)
+  memos << { id: SecureRandom.uuid, title: title, body: body }
   File.write(filename, JSON.pretty_generate(memos))
   memos.last
 end
 
 def read_memo_list
   File.open('memo_list.json', 'a') { |file| file.puts '[]' } unless File.exist?('memo_list.json')
-  JSON.parse(File.read('memo_list.json'))
+  JSON.parse(File.read('memo_list.json'), symbolize_names: true)
 end
 
 def delete_memo(filename, id)
   memos = JSON.parse(File.read(filename))
-  memos.reject! { |memo| memo['id'] == id }
+  memos.reject! { |memo| memo[:id] == id }
   File.write(filename, JSON.pretty_generate(memos))
 end
 
@@ -38,7 +38,7 @@ end
 before '/memos/:id*' do
   # '/memos/new' の場合はフィルターをスキップし、それ以外はメモを探して見つからなければ 404 を返す
   pass if params[:id] == 'new'
-  @memo = @memos.find { |memo| memo['id'] == params[:id] }
+  @memo = @memos.find { |memo| memo[:id] == params[:id] }
   halt 404, erb(:not_found) unless @memo
 end
 
@@ -55,8 +55,8 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  memo = store_memo('memo_list.json', params['title'], params['body'])
-  redirect "/memos/#{memo['id']}"
+  memo = store_memo('memo_list.json', params[:title], params[:body])
+  redirect "/memos/#{memo[:id]}"
 end
 
 get '/memos/:id' do
@@ -68,14 +68,14 @@ get '/memos/:id/edit' do
 end
 
 patch '/memos/:id' do
-  @memo['title'] = params['title']
-  @memo['body']  = params['body']
+  @memo[:title] = params[:title]
+  @memo[:body]  = params[:body]
   File.write('memo_list.json', JSON.pretty_generate(@memos))
-  redirect "/memos/#{@memo['id']}"
+  redirect "/memos/#{@memo[:id]}"
 end
 
 delete '/memos/:id' do
-  delete_memo('memo_list.json', @memo['id'])
+  delete_memo('memo_list.json', @memo[:id])
   redirect '/'
 end
 
